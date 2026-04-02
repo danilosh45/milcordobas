a16b1aae33f2c2fd0eda730fe79b967a2042c7ef
Danilo Nora <danilosh45@gmail.com>
2026-04-02 12:23:55 +0200

    chore: fix grammar footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -30,7 +30,7 @@ export function Footer() {
               Mil Córdobas
             </Text>
             <Text color="gray.500" fontSize="sm">
-              Rock desde Alfaro desde el 2019 
+              Rock desde Alfaro 2019 
             </Text>
           </VStack>
 
